b69e59d2e225e526938c40cde20610070ee1a44f
Colton Gagel <coltongagel@gmail.com>
2022-10-11 14:32:11 -0700

    Create tadm_mdb.bat

---
+++ tadm_mdb.bat
@@ -0,0 +1,7 @@
+@echo off
+python "C:\Program Files (x86)\HAMILTON\Methods\TADM\tadm_mdb.py" "%*"
+pause
+exit /b %ERRORLEVEL%
+
+
+:: python "C:\Program Files (x86)\HAMILTON\Methods\TADM\tadm_mdb.py" "C:/Program Files (x86)/HAMILTON/LogFiles/HSL Reagent Fill timeout testing_b663f85120424236af6675da4ed4c710_ML_STAR_tadm.mdb"
